e4435cf134d9f6e625306108ce909d94dc83758d
BuildTools <Junwoo0806@hotmail.com>
2019-02-22 22:12:56 +0900

    Nothing changed

--- src/containers/Account/index.js
+++ src/containers/Account/index.js
@@ -137,6 +137,7 @@ export default function Account({ login }) {
           <select><option>---Select Month---</option><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option><option>6</option><option>7</option><option>8</option><option>9</option><option>10</option><option>11</option><option>12</option></select>
           <input placeholder="Year" />
           <br />
+          <p>By clicking sign up the you have read and agreed to <a href="/SUPIA" target="_blank">{`"Collection and use of personal information"`}</a></p>
           <button className="button" onClick={signUp}>
             Sign Up
           </button>
