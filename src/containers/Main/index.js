86ac1771c74d9f4a09c81de7e3542d599ac28a17
mikey1384 <mikey1384@gmail.com>
2019-05-03 19:39:27 +0800

    minor change for testing

--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -19,7 +19,7 @@ export default function Main({ history, userId, username, logout }) {
       <div className="App-header">
         {!userId && (
           <div>
-            <h1>WELCOME</h1>
+            <h1>WELCOME!</h1>
             <h2>{`to the DMJW's FUNonline and Developer Tools website!`}</h2>
             <h3>{`DMJW's Fun온라인, 개발자 도구 사이트에 오신것을 환영합니다!`}</h3>
             <h3>{`DMJW's Fun オンライン, 開発者サイトへようこそ!`}</h3>
@@ -32,7 +32,7 @@ export default function Main({ history, userId, username, logout }) {
         )}
         {userId && (
           <div>
-            <h1>WELCOME {username}</h1>
+            <h1>WELCOME {username}!</h1>
             <h2>{`to the DMJW's FUNonline and Developer Tools website!`}</h2>
             <h3>{`DMJW's Fun온라인, 개발자 도구 사이트에 오신것을 환영합니다!`}</h3>
             <h3>{`DMJW's Fun オンライン, 開発者サイトへようこそ!`}</h3>
@@ -71,10 +71,7 @@ export default function Main({ history, userId, username, logout }) {
         <p>Now : English - UK, US</p>
       </Link>
       {userId && (
-        <button
-          style={{ fontSize: "17pt" }}
-          onClick={logout}
-        >
+        <button style={{ fontSize: "17pt" }} onClick={logout}>
           Log Out 🔐
         </button>
       )}
